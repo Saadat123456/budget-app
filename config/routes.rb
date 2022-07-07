f60374859e0a5942985ccbb45818a2681156edac
Saadat Ali <saadatlucky313@gmail.com>
2022-07-07 09:12:37 +0500

    Add routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  get '/users/:user_id', to: "splash#index", as: 'user'
 
   # Defines the root path route ("/")
   root "splash#index"
