8e1ad3a86ee333d9aff553dfe0a29467545fd239
Saadat Ali <saadatlucky313@gmail.com>
2022-07-08 17:35:54 +0500

    Implement actions for budgetting transactions

--- app/controllers/budgetting_transactions_controller.rb
+++ app/controllers/budgetting_transactions_controller.rb
@@ -1,7 +1,26 @@
 class BudgettingTransactionsController < ApplicationController
-  def index; end
+  def index
+    category_id = params[:category_id]
+    @transactions = BudgettingTransaction.where(group_id: category_id)
+  end
 
-  def new; end
+  def new
+    @category_id = params[:category_id]
+  end
 
-  def create; end
+  def create
+    transaction = BudgettingTransaction.new budgetting_transaction_params
+    transaction.user = current_user
+    transaction.group_id = params[:category_id]
+
+    return unless transaction.save
+
+    respond_to do |f|
+      f.html { redirect_to category_budgetting_transactions_path }
+    end
+  end
+
+  def budgetting_transaction_params
+    params.require(:budgetting_transactions).permit(:name, :amount)
+  end
 end
