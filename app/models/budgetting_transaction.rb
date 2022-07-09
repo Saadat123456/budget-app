3ddcef4c4c7460f3e6783eb18544c450d2704547
Saadat Ali <saadatlucky313@gmail.com>
2022-07-09 14:24:16 +0500

    Add validations for budgetting transactions

--- app/models/budgetting_transaction.rb
+++ app/models/budgetting_transaction.rb
@@ -1,4 +1,10 @@
 class BudgettingTransaction < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates :name, presence: { message: "Name can't be blank" }
+  validates :user, presence: true
+  validates :group, presence: true
+  validates :amount, presence: true, numericality:
+    { only_float: true, greater_than: 0, message: 'Amount must be greater than 0' }
 end
