2ddd660283a62f4a725e6fc1b521ac3e73debaa4
Saadat Ali <saadatlucky313@gmail.com>
2022-07-09 14:24:31 +0500

    Add validations for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :budgetting_transactions
+
+  validates :name, presence: { message: 'Name can\'t be blank' }
+  validates :user, presence: true
 end
